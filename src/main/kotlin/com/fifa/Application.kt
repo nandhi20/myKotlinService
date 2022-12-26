a537d57f3d4597d984c5f75271888955994bb26a
Nandhini Viswanathan <nandhinivishwanathanbe@gmail.com>
2022-12-26 12:44:21 +0530

    Adding Routes

--- src/main/kotlin/com/fifa/Application.kt
+++ src/main/kotlin/com/fifa/Application.kt
@@ -3,9 +3,17 @@ package com.fifa
 import com.fifa.Repository.CountryRepository
 import com.fifa.Repository.Impl.CountryRepositoryImpl
 import com.fifa.db.DatabaseFactory
+import com.fifa.models.CountryParams
 import com.fifa.service.CountryService
+
 import com.fifa.service.impl.CountryServiceImpl
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.application.*
+import io.ktor.serialization.jackson.*
 import io.ktor.server.engine.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.tomcat.*
 
 
@@ -14,10 +22,22 @@ fun main() {
 
         DatabaseFactory.init() //Initializing database
 
+        //Content Negotiation and Serialization
+        install(ContentNegotiation) {
+            jackson()
+        }
+        //Routings
         val service: CountryService = CountryServiceImpl()
         val repository: CountryRepository = CountryRepositoryImpl(service)
-//        countryRoutes(repository)
-//        authRoutes(repository)
+        routing {
+            route("/country") {
+                post("/register") {
+                    val params = call.receive<CountryParams>()
+                    val result = repository.registerCountry(params)
+                    call.respond(result.statusCode, result)
+                }
+            }
+        }
     }
         .start(wait = true)
 }
